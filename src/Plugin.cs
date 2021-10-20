54160ed545ff44532d42c7943deb81345a377fed
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2021-10-19 22:53:10 -0400

    Fix formatting

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -8,32 +8,32 @@ public sealed class Plugin : BaseUnityPlugin
 {
     public void OnEnable()
     {
-		static IEnumerable<Fisob> GetFisobs()
+        static IEnumerable<Fisob> GetFisobs()
         {
-			yield return CentiShieldFisob.Instance;
+            yield return CentiShieldFisob.Instance;
         }
 
-		FisobRegistry reg = new(GetFisobs());
+        FisobRegistry reg = new(GetFisobs());
 
-		reg.ApplyHooks();
+        reg.ApplyHooks();
 
-		On.Room.AddObject += Room_AddObject;
-	}
+        On.Room.AddObject += Room_AddObject;
+    }
 
-	private void Room_AddObject(On.Room.orig_AddObject orig, Room self, UpdatableAndDeletable obj)
-	{
-		if (obj is CentipedeShell shell && shell.scaleX > 0.9f && shell.scaleY > 0.9f && UnityEngine.Random.value < 0.25f) {
-			var tilePos = self.GetTilePosition(shell.pos);
-			var pos = new WorldCoordinate(self.abstractRoom.index, tilePos.x, tilePos.y, 0);
-			var abstr = new CentiShieldAbstract(self.world, pos, self.game.GetNewID()) {
-				hue = shell.hue,
-				saturation = shell.saturation,
-				scaleX = shell.scaleX,
-				scaleY = shell.scaleY
-			};
-			obj = new CentiShield(abstr, shell.pos, shell.vel);
-		}
+    private void Room_AddObject(On.Room.orig_AddObject orig, Room self, UpdatableAndDeletable obj)
+    {
+        if (obj is CentipedeShell shell && shell.scaleX > 0.9f && shell.scaleY > 0.9f && UnityEngine.Random.value < 0.25f) {
+            var tilePos = self.GetTilePosition(shell.pos);
+            var pos = new WorldCoordinate(self.abstractRoom.index, tilePos.x, tilePos.y, 0);
+            var abstr = new CentiShieldAbstract(self.world, pos, self.game.GetNewID()) {
+                hue = shell.hue,
+                saturation = shell.saturation,
+                scaleX = shell.scaleX,
+                scaleY = shell.scaleY
+            };
+            obj = new CentiShield(abstr, shell.pos, shell.vel);
+        }
 
-		orig(self, obj);
-	}
+        orig(self, obj);
+    }
 }
